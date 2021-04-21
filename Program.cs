2c08cc11290f652773a7867c290cfe789583633f
Matheus_Sulzbach <Matheus_Sulzbach@W10HM3LYY2>
2021-04-20 22:16:02 -0300

    ok

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace nu_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console App Running!");
+
+            Thread threadProducer = new Thread(new ThreadStart(Producer.Run));
+            threadProducer.Start();
+
+            Thread threadConsumer = new Thread(new ThreadStart(Consumer.Run));
+            threadConsumer.Start();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 "# nu-test" 
 1. ensure that you have docker running 
 2. run docker-compose up -d in the same path as docker-compose.yml file
-3. 
+3. run dotnet run
